b8141fef9bc1ae260891f62ec82d27a69f7b3377
Chase <cabbott93@gmail.com>
2021-05-10 16:46:41 -0700

    add player detail

--- src/detail/PlayerDetail.js
+++ src/detail/PlayerDetail.js
@@ -28,6 +28,12 @@ export default class PlayerDetail extends Component {
       <div className="PlayerDetail">
         <h2> {player.name} </h2>
         <img src={player.url_image} alt={player.name}></img>
+        <h4> {player.position}</h4>
+        <h4> Enrolled: {player.yearEnrolled}</h4> 
+        { player.isTransfer && <h4 className="transfer"> Transfer </h4>}
+        { !player.isTransfer && <h4 className="signee"> Signee </h4>}
+        { player.isActive && <h4 className="active"> Active</h4>}
+        { !player.isActive && <h4 className="graduated"> Graduated </h4>}
       </div>
     );
   }
